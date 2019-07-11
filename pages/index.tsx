c6fb7913ea444cc5b7333d1aa9cdd48b910080ba
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-07-11 17:42:46 +0900

    init prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,15 @@
-import * as React from 'react'
-import Typography from '@material-ui/core/Typography';
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Typography from "@material-ui/core/Typography";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
-    return (
-        <Layout title="Home | Next.js + TypeScript Example">
-            <h1>Hello Next.js 👋</h1>
-            <Typography variant="body2">hello</Typography>
-            <p>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-            </p>
-        </Layout>
-    )
-}
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <Typography variant="body2">hello</Typography>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
