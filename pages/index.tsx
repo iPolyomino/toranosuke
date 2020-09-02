d4f548079359521f32d1f05822c5de371521c46e
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-09-03 01:24:19 +0900

    feat: google translate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Layout from "../components/Layout";
 import IndexCard from "../components/IndexCard";
@@ -7,16 +7,31 @@ import { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
   const [text, setText] = useState("");
+  const [gtext, setGtext] = useState("");
 
   const updateText = (text: string): void => {
     setText(text);
   };
 
+  useEffect(() => {
+    const url =
+      "https://script.google.com/macros/s/AKfycbwW5mcKD7UaFboSHFML6fcTUk7EmjrCI5lNEMATGV_-o2sB2Rs/exec";
+    fetch(`${url}?text=${text}&source=ja&target=en`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.code === 200) {
+          setGtext(result.text);
+        } else {
+          setGtext("");
+        }
+      });
+  });
+
   return (
     <Layout title="とらのすけ">
       <Typography variant="h3">とらのすけ - toranosuke</Typography>
       <IndexTextField updateText={updateText} />
-      <IndexCard title="Input Text" description={text} />
+      <IndexCard title="Google 翻訳" description={gtext} />
     </Layout>
   );
 };
